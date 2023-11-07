b95b359c997aabbb76ccb04511e25133ae41ba73
alissonthx <95230723+alissonthx@users.noreply.github.com>
2023-11-06 23:38:56 -0300

    add: stove counter script

--- Assets/Scripts/Counters/ClearCounter.cs
+++ Assets/Scripts/Counters/ClearCounter.cs
@@ -3,11 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Counter : BaseCounter
+public class ClearCounter : BaseCounter
 {
-    [SerializeField] private KitchenObjectSO kitchenObjectSO;
-
-
     // interact to instantiate food in blocks, need to change the object parent to player parent
     public override void Interact(Player player)
     {
---
+++ Assets/Scripts/Counters/StoveCounter.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoveCounter : BaseCounter
+{
+    public override void Interact(Player player)
+    {
+        
+    }
+}
