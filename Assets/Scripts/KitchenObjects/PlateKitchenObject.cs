44ac25f32c80fef741ad15d0d7e0c0ad4e631738
alissonthx <95230723+alissonthx@users.noreply.github.com>
2023-12-09 16:23:02 -0300

    add: icon visual to plates

--- Assets/Scripts/KitchenObjects/PlateKitchenObject.cs
+++ Assets/Scripts/KitchenObjects/PlateKitchenObject.cs
@@ -41,4 +41,8 @@ public class PlateKitchenObject : KitchenObject
             return true;
         }
     }
+
+    public List<KitchenObjectSO> GetKitchenObjectSOList() {
+        return kitchenObjectSOList;
+    }
 }
--- Assets/Scripts/KitchenObjects/PlatesIconUI.cs
+++ Assets/Scripts/KitchenObjects/PlatesIconUI.cs
@@ -1,8 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using Microsoft.Unity.VisualStudio.Editor;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlatesIconUI : MonoBehaviour
 {
     [SerializeField] private PlateKitchenObject plateKitchenObject;
+    [SerializeField] private Transform iconTemplate;
+
+    private void Awake(){
+        iconTemplate.gameObject.SetActive(false);
+    }
+    private void Start(){
+        plateKitchenObject.OnIngredientAdded += PlateKitchenObject_OnIngredientAdded;
+    }
+
+    private void PlateKitchenObject_OnIngredientAdded(object sender, PlateKitchenObject.OnIngredientAddedEventArgs e)
+    {
+        UpdateVisual();
+    }
+
+    private void UpdateVisual(){
+        foreach(Transform child in transform){
+            if(child == iconTemplate) continue;
+            Destroy(child.gameObject);
+        }
+
+        foreach(KitchenObjectSO kitchenObjectSO in plateKitchenObject.GetKitchenObjectSOList()){
+            Transform iconTransform = Instantiate(iconTemplate, transform);  
+            iconTemplate.gameObject.SetActive(true);
+            iconTransform.GetComponent<PlateIconSingleUI>().SetKitchenObjectSO(kitchenObjectSO);          
+        }
+    }
 }
