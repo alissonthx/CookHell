83e6c0fe96b4d4e12ddfd908f2592f9799dbc32d
alissonthx <95230723+alissonthx@users.noreply.github.com>
2023-12-09 15:27:13 -0300

    add: add hamburguer complete on plates visual logic

---
+++ Assets/Scripts/KitchenObjects/PlatesIconUI.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatesIconUI : MonoBehaviour
+{
+    [SerializeField] private PlateKitchenObject plateKitchenObject;
+}
