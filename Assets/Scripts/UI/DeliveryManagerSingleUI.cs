74516f54bb6cf796d040cab977646869dd748f34
alissonthx <95230723+alissonthx@users.noreply.github.com>
2023-12-12 20:56:01 -0300

    add: circle select on player

--- Assets/Scripts/UI/DeliveryManagerSingleUI.cs
+++ Assets/Scripts/UI/DeliveryManagerSingleUI.cs
@@ -1,18 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class DeliveryManagerSingleUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private TextMeshProUGUI recipeNameText;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void SetRecipeSO(RecipeSO recipeSO){
+        recipeNameText.text = recipeSO.recipeName;
     }
 }
