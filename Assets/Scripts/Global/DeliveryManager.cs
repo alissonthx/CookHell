e6846f7ca77dc70921de76685fcb8be68bb07a39
alissonthx <95230723+alissonthx@users.noreply.github.com>
2023-12-13 13:18:48 -0300

    add: music and ambience sounds

--- Assets/Scripts/Global/DeliveryManager.cs
+++ Assets/Scripts/Global/DeliveryManager.cs
@@ -9,7 +9,8 @@ public class DeliveryManager : MonoBehaviour
 
     public event EventHandler OnRecipeSpawned;
     public event EventHandler OnRecipeCompleted;
-
+    public event EventHandler OnRecipeSuccess;    
+    public event EventHandler OnRecipeFailed;
     public static DeliveryManager Instance { get; private set; }
 
     [SerializeField] private RecipeListSO recipeListSO;
@@ -78,14 +79,15 @@ public class DeliveryManager : MonoBehaviour
                     waitingRecipeSOList.RemoveAt(i);
 
                     OnRecipeCompleted?.Invoke(this, EventArgs.Empty);
+
+                    OnRecipeSuccess?.Invoke(this, EventArgs.Empty);
                     return;
                 }
             }
         }
         // no matches found
         // player did not deliver a correct recipe
-        Debug.Log("Player not delivered the correct recipe");
-
+        OnRecipeFailed?.Invoke(this, EventArgs.Empty);
     }
 
     public List<RecipeSO>GetWaitingRecipeSOList(){
---
+++ Assets/Scripts/Global/SoundManager.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField] private AudioClipRefsSO audioClipRefsSO;
+    private void Start()
+    {
+        DeliveryManager.Instance.OnRecipeSuccess += DeliveryManager_OnRecipeSuccess;
+        DeliveryManager.Instance.OnRecipeFailed += DeliveryManager_OnRecipeFailed;
+    }
+
+    private void DeliveryManager_OnRecipeSuccess(object sender, EventArgs e)
+    {
+        // PlaySound(AudioClipRefsSO.deliveryFail, Camera.main.transform.position);
+    }
+
+    private void DeliveryManager_OnRecipeFailed(object sender, EventArgs e)
+    {
+        // PlaySound(AudioClipRefsSO.deliverySucess, Camera.main.transform.position);
+    }
+
+    private void PlaySingleSound(AudioClip audioClip, Vector3 position, float volume = 1f)
+    {
+        AudioSource.PlayClipAtPoint(audioClip, position, volume);
+    }
+    private void PlaySound(AudioClip[] audioClipArray, Vector3 position, float volume = 1f)
+    {
+        AudioSource.PlayClipAtPoint(audioClipArray[UnityEngine.Random.Range(0, audioClipArray.Length)], position, volume);
+    }
+}
